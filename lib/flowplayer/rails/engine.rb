1d11edf0073b5944b0f29cf434060378472d3bc0
ryancheung <ryancheung.go@gmail.com>
2014-03-04 12:42:23 +0800

    Added assets to precompile path

--- lib/flowplayer/rails/engine.rb
+++ lib/flowplayer/rails/engine.rb
@@ -4,6 +4,11 @@ module Flowplayer
       config.to_prepare do
         ApplicationController.helper(Flowplayer::Rails::Helper)
       end
+
+      initializer "flowplayer.assets.precompile" do |app|
+        app.config.assets.precompile += %w(flowplayer.min.js)
+        app.config.assets.precompile += %w(*.swf)
+      end
     end
   end
 end
